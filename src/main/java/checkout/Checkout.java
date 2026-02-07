7e6467e3f09f57b98cf7740f8c6bb43c1a4b9328
Ritankar Das <ritankar15@gmail.com>
2026-02-07 18:41:11 +0000

    Basic checkout calculates total price

--- src/main/java/checkout/Checkout.java
+++ src/main/java/checkout/Checkout.java
@@ -1,4 +1,22 @@
 package checkout;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
 public class Checkout {
+
+    private static final Map<String, Integer> pricesInPence = Map.of(
+            "Apple", 60,
+            "Orange", 25
+    );
+
+    public BigDecimal calculateTotal(List<String> items) {
+        int totalInPence = items.stream()
+                .mapToInt(item -> pricesInPence.get(item))
+                .sum();
+
+        return BigDecimal.valueOf(totalInPence)
+                .divide(BigDecimal.valueOf(100));
+    }
 }
--- src/test/java/checkout/CheckoutTest.java
+++ src/test/java/checkout/CheckoutTest.java
@@ -1,4 +1,22 @@
 package checkout;
 
-public class CheckoutTest {
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CheckoutTest {
+
+    @Test
+    void calculates_total_cost_without_offers() {
+        Checkout checkout = new Checkout();
+
+        BigDecimal total = checkout.calculateTotal(
+                List.of("Apple", "Apple", "Orange", "Apple")
+        );
+
+        assertThat(total).isEqualByComparingTo("2.05");
+    }
 }
